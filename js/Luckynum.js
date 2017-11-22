9ab00ed9fdf82d651974b1d13f519963da027d62
严政 <yanzheng@lemediaweb.com>
2017-11-22 19:12:50 +0800

    perf: setTimeout代替setInterval

--- js/Luckynum.js
+++ js/Luckynum.js
@@ -6,16 +6,20 @@
 class Luckynum{
 
     constructor(config){
+        this._id=0;
         this._topArr=[-1032,-912,-796,-678,-560,-440,-322,-204,-86,-1150];
         this._myDom=config.dom;
         this._luckNum=[];
         this._runUpTimer=[];
         this._speed=[];
         this._flag=[];
-        this._check=[]
+        this._check=[];
+        this._checkAgain=[];
         this._run=false;
+        this._top=[];
         this.addHtml();
 
+       
         let start = document.getElementsByName('start')[0];
         start.addEventListener('click', () => {
             this.startRun()
@@ -47,21 +51,23 @@ class Luckynum{
     startRun(){
         if(!this._run) {
             console.log('start run')
+            this._run=true;
             this.runUp(0)
             this.runUp(1)
             this.runUp(2)
-            this._run=true;
+
         }
+
 
     }
 
-    isNow(top,index) {
+    isNow(index) {
         let num=this._luckNum[index]
-       // console.log(num);
+       //console.log(num);
        // console.log(top+' '+this._topArr[num])
         switch (index){
             case 2: if(!this._flag[1]) {
-                        return false
+                       // return false
                     }else {
                          this._speed[index]=4
                         if(this._check[index])
@@ -69,7 +75,7 @@ class Luckynum{
                     }
                     break;
             case 1: if(!this._flag[0]) {
-                          return false
+                        //  return false
                     }else {
                           this._speed[index]=4
                           if(this._check[index])
@@ -82,7 +88,7 @@ class Luckynum{
         }
 
 
-        if(Math.abs(top-this._topArr[num])<5)
+        if(Math.abs(this._top[index]-this._topArr[num])<5)
         {
             if(this._check[index])
             {
@@ -93,7 +99,7 @@ class Luckynum{
             else
             {
                 if(this._speed[index]===4){
-                 //  console.log(index," get")
+                  console.log(index," get",this._top[index])
                     this._check[index]=true
                     this._speed[index]=2
                     //console.log(this._speed[index])
@@ -110,63 +116,62 @@ class Luckynum{
         this._flag=[];
         this._check=[];
         this._run=false;
+        this._checkAgain=[]
+
 
     }
-    run(top,index){
-
-
+    run(img,index){
+       // this._id++;
+       // console.log(this._id++);
+        if(this._speed[index]===undefined)
+        {
+            console.log(this)
+            return
+        }
+        if(this._luckNum[index]!==undefined&&this.isNow(index))
+        {
+             //console.log(this._luckNum)
+           // clearInterval(this._runUpTimer[index])
+            this._flag[index]=true;
+            this._checkAgain[index]=true;
+        }
+        //console.log(img)
+        if(this._top[index]<=-1185){
+            // clearInterval(aa)
+            this._top[index]=0;
+        }
+        img.style.top = this._top[index] + 'px';
+        this._top[index] -= this._speed[index];
+       // console.log(index,this._top[index])
+        // clearTimeout(timer1)
+        //  setTimeout(this.runUp,10)
+    }
+    runfun(img,index) {
+        this.run(img,index);
+        //console.log(this._run)
+        if(!this._checkAgain[index]){
+            setTimeout(()=>{
+               // console.log(this._run)
+              this.runfun(img,index)
+            },10);
+        }else {
+            if(this._flag.filter(
+                    (b)=>{
+                        return b
+                    }
+                ).length===3)
+            {
+                console.log('reSet')
+                this.reSet()
+                this._run=false
+            }
+        }
     }
     runUp(index) {
-
         let img=document.getElementById('img'+(index+1))
-        let top = img.style.top.split('px')[0];
-        let self = this;
+        this._top[index]= img.style.top.split('px')[0];
         this._speed[index] =10;
-       /* function tick(self) {
-                self.run();
-                num++;
-                if(num > self._round*8){
-                    interval(tick2(self), 300);
-                } else {
-                    interval(tick(self), 100);
-                }
-            }
-        function interval(callback, interval) {
-                 setTimeout(callback, interval);
-        }*/
-
-        this._runUpTimer[index]=setInterval(()=>{
-
-
-            if(this._luckNum[index]!==undefined&&this.isNow(top,index))
-            {
-               // console.log(this._luckNum)
-                clearInterval(this._runUpTimer[index])
-                this._flag[index]=true;
-                if(this._flag.filter(
-                        (b)=>{
-                            return b
-                        }
-                    ).length===3)
-                {
-                    console.log('reSet')
-                    this.reSet()
-                }
-
-
-            }
-            //console.log(img)
-            if(top<=-1185){
-                // clearInterval(aa)
-                top=0;
-            }
-            img.style.top = top + 'px';
-            top -= this._speed[index];
-            // clearTimeout(timer1)
-          //  setTimeout(this.runUp,10)
-        },10)
-
-
+        this.runfun(img,index)
 
     }
 }
