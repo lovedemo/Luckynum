ca198b2af6262ad897f225d0bcbefefb6af7046d
严政 <yanzheng@lemediaweb.com>
2017-11-27 10:51:16 +0800

    docs: 为代码添加注释

--- js/Luckynum.js
+++ js/Luckynum.js
@@ -6,35 +6,49 @@
 export class Luckynum{
 
     constructor(config){
-        this._id=0;
-        this._topArr=[-1032,-912,-796,-678,-560,-440,-322,-204,-86,-1150];
-        this._myDom=config.dom;
-        this._luckNum=[];
-        this._runUpTimer=[];
-        this._speed=[];
-        this._flag=[];
+        this._topArr=[-1032,-912,-796,-678,-560,-440,-322,-204,-86,-1150];// 0-9数字对应的top值
+        this._myDom=config.dom; //传入dom
+        this._luckNum=[];  //抽奖结果
+        this._speed=[]; //各个图片转动的速度
+        this._flag=[]; // 每个图片是否已停止
+
+        // 为实现慢慢停下来的效果 两次到达后才停下来
         this._check=[];
         this._checkAgain=[];
-        this._run=false;
-        this._top=[];
+
+
+        this._run=false;// 数字转动后，让开始按钮不可触发
+        this._top=[];// 当前图片所处位置
         this.addHtml();
 
+        //为按钮添加点击事件
         let start = document.getElementsByName('start')[0];
         start.addEventListener('click', () => {
-            this.startRun()
+            this.startRun();
         });
     }
+    /*
+    * 获得一个随机的初始值
+    *
+    * */
     radomStart() {
         for(let i=0;i<3;i++){
-
-            document.getElementById('img'+(i+1)).style.top=this._topArr[Math.floor(Math.random()*10)]+'px'
-
+            document.getElementById('img'+(i+1)).style.top=this._topArr[Math.floor(Math.random()*10)]+'px';
         }
     }
+
+    /*
+    * 设置获奖数字，可从服务端获取
+    *
+    **/
     setNum(num) {
         this._luckNum=(num+'').split('');
-       // console.log(this._luckNum)
     }
+
+    /*
+    * 初始化dom
+    *
+    * */
     addHtml(){
         let div = document.createElement('div');
         div.setAttribute('id','luckynum');
@@ -47,131 +61,154 @@ export class Luckynum{
         this._myDom.appendChild(div);
         this.radomStart();
     }
+    /**
+    * 开始转动
+    *
+    **/
     startRun(){
         if(!this._run) {
+            this.reSet();
             console.log('start run')
             this._run=true;
-            this.runUp(0)
-            this.runUp(1)
-            this.runUp(2)
+            this.runUp(0);
+            this.runUp(1);
+            this.runUp(2);
+            setTimeout(()=>{
+                    if(this._luckNum.length===0)
+                    {
+                        this._checkAgain=[true,true,true];
+                        alert('抽奖超时');
+                        this._run=false;
+                       // this.radomStart();
+                       // this.reSet();
+
+                    }
+            },10000)
 
         }
 
 
     }
 
-    isNow(index) {
-        let num=this._luckNum[index]
-       //console.log(num);
-       // console.log(top+' '+this._topArr[num])
-        switch (index){
-            case 2: if(!this._flag[1]) {
-                       // return false
-                    }else {
-                         this._speed[index]=4
-                        if(this._check[index])
-                            this._speed[index]=2
+    /*
+    * 根据传入的index转动对应的img
+    *
+    * */
+    runUp(index) {
+        let img=document.getElementById('img'+(index+1));
+        this._top[index]= img.style.top.split('px')[0];
+        this._speed[index] =10;
+        this.runfun(img,index)
+
+    }
+
+    /*
+    * 控制循环的函数，当3个img都停下来就重置
+    * 用setimeout代替setInterval
+    *
+    * */
+    runfun(img,index) {
+        this.run(img,index);
+        if(!this._checkAgain[index]){
+            setTimeout(()=>{
+                this.runfun(img,index)
+            },10);
+        }else {
+            if(this._flag.filter(
+                    (b)=>{
+                        return b;
                     }
-                    break;
-            case 1: if(!this._flag[0]) {
-                        //  return false
-                    }else {
-                          this._speed[index]=4
-                          if(this._check[index])
-                             this._speed[index]=2
-                    }
-                    break;
-            case 0: this._speed[index]=4
-                    if(this._check[index])
-                        this._speed[index]=2
+                ).length===3)
+            {
+                console.log(this._luckNum);
+                console.log('reSet');
+                this.reSet();
+
+            }
+        }
+    }
+
+    /*
+    * 循环函数，每次转动的速度为speed[index]
+    *
+    * */
+    run(img,index){
+        /*if(this._speed[index]===undefined)
+        {
+            console.log(this);
+            return;
+        }*/
+       // console.log(index);
+        if(this._luckNum[index]!==undefined&&this.isNow(index))
+        {
+            this._flag[index]=true;
+            this._checkAgain[index]=true;
         }
 
+        if(this._top[index]<=-1185){
+            this._top[index]=0;
+        }
+        img.style.top = this._top[index] + 'px';
+        this._top[index] -= this._speed[index];
+    }
 
+    /*
+    * 判断当前是否应该停下
+    * return true/false
+    * */
+    isNow(index) {
+        let num=this._luckNum[index];
+
+        //前一个停下后速度才变为4，到达一次后速度变为2
+        switch (index){
+            case 2: if(this._flag[1]) {
+                         this._speed[index]=4;
+                        if(this._check[index])
+                            this._speed[index]=2;
+                    }
+                    break;
+            case 1: if(this._flag[0]) {
+                          this._speed[index]=4;
+                          if(this._check[index])
+                             this._speed[index]=2;
+                    }
+                    break;
+            case 0: this._speed[index]=4;
+                    if(this._check[index])
+                        this._speed[index]=2;
+        }
+
+        //到达对应的位置
         if(Math.abs(this._top[index]-this._topArr[num])<5)
         {
+            //如果第二次到达就停下来，否则就标记一次到达
             if(this._check[index])
             {
                // console.log(this._speed[index])
-                return true
+                return true;
             }
-
             else
             {
                 if(this._speed[index]===4){
-                //  console.log(index," get",this._top[index])
-                    this._check[index]=true
-                    this._speed[index]=2
-                    //console.log(this._speed[index])
+                    this._check[index]=true;
+                    this._speed[index]=2;
                 }
-
             }
         }
-        return false
+        return false;
     }
+
+    /*
+    * 重置所有参数
+    *
+     * */
     reSet() {
         this._luckNum=[];
-        this._runUpTimer=[];
         this._speed=[];
         this._flag=[];
         this._check=[];
         this._run=false;
         this._checkAgain=[]
-
-
-    }
-    run(img,index){
-       // this._id++;
-       // console.log(this._id++);
-        if(this._speed[index]===undefined)
-        {
-            console.log(this)
-            return
-        }
-        if(this._luckNum[index]!==undefined&&this.isNow(index))
-        {
-             //console.log(this._luckNum)
-           // clearInterval(this._runUpTimer[index])
-            this._flag[index]=true;
-            this._checkAgain[index]=true;
-        }
-        //console.log(img)
-        if(this._top[index]<=-1185){
-            // clearInterval(aa)
-            this._top[index]=0;
-        }
-        img.style.top = this._top[index] + 'px';
-        this._top[index] -= this._speed[index];
-       // console.log(index,this._top[index])
-        // clearTimeout(timer1)
-        //  setTimeout(this.runUp,10)
-    }
-    runfun(img,index) {
-        this.run(img,index);
-        //console.log(this._run)
-        if(!this._checkAgain[index]){
-            setTimeout(()=>{
-               // console.log(this._run)
-              this.runfun(img,index)
-            },10);
-        }else {
-            if(this._flag.filter(
-                    (b)=>{
-                        return b
-                    }
-                ).length===3)
-            {
-                console.log(this._luckNum)
-                console.log('reSet')
-                this.reSet()
-                this._run=false
-            }
-        }
-    }
-    runUp(index) {
-        let img=document.getElementById('img'+(index+1))
-        this._top[index]= img.style.top.split('px')[0];
-        this._speed[index] =10;
-        this.runfun(img,index)
 
     }
 }
--- js/import.js
+++ js/import.js
@@ -6,8 +6,8 @@
 import{ Luckynum } from '../js/Luckynum'
 let obj={};
 obj.dom=document.getElementById('app');
-let aa=new Luckynum(obj);
-console.log(aa)
- window.aa=aa;
+let luckynum=new Luckynum(obj);
+console.log(luckynum)
+window.luckynum=luckynum;
 
 
